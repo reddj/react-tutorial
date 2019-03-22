0ad6630c3ea9c3b93f7ebec50542fae08291fb40
Johnathan <reddjohnathan@gmail.com>
2019-03-22 11:56:15 -0500

    making progress

--- src/index.js
+++ src/index.js
@@ -2,31 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-class Square extends React.Component {
-    render() {
+function Square(props) {
         return(
             <button 
               className='square' 
-              onClick={()=> this.props.onClick()}
+              onClick={props.onClick}
             >
-              {this.props.value}
+              {props.value}
             </button>
         )
     }
-}
+
 
 class Board extends React.Component {
     constructor(props) {
       super(props)
       this.state = {
           squares: Array(9).fill(null),
+          xIsNext: true
       }
     }
 
     handleClick(i){
         const squares = this.state.squares.slice()
-        squares[i] = 'X'
-        this.setState({squares: squares})
+        squares[i] = this.state.xIsNext ? 'X' : 'O'
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        })
     }
     renderSquare(i) {
         return (
